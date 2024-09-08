1708d6db7358e2fa4830e8be113b8c0892e3d0ca
bijay-05 <hawkeyesniper69@gmail.com>
2024-09-08 16:13:12 +0545

    feat: exception filters

--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,11 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import { AppExceptionFilter } from './filters/app-exception.filter';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+
+  app.useGlobalFilters(new AppExceptionFilter());
   await app.listen(3000);
 }
 bootstrap();
