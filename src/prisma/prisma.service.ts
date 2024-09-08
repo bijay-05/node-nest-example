9a32aeb81e86dc88000f23beb3adc3d8a6a70e81
bijay-05 <hawkeyesniper69@gmail.com>
2024-09-08 13:25:18 +0545

    feat: create prisma service

---
+++ src/prisma/prisma.service.ts
@@ -0,0 +1,25 @@
+import { PrismaClient } from '@prisma/client';
+import { Injectable, OnModuleInit } from '@nestjs/common'
+
+@Injectable()
+export class PrismaService extends PrismaClient implements OnModuleInit {
+    constructor() {
+        super({
+          datasources: {
+            db: {
+              url: process.env.DATABASE_URL,
+            },
+          },
+        });
+      }
+    
+      async onModuleInit() {
+        await this.$connect()
+          .then(() => console.log('Database connected successfully..'))
+          .catch((err) => console.log('Unable to connect database..', err));
+      }
+    
+      async onModuleDestroy() {
+        await this.$disconnect();
+      }
+}
