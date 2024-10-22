5827255db84344024988da42927927fc7932b6be
bijay-05 <hawkeyesniper69@gmail.com>
2024-10-22 22:04:02 +0545

    feat: add users model prisma

--- prisma/migrations/20240908073059_add_posts_schema/migration.sql
+++
@@ -1,9 +0,0 @@
--- CreateTable
-CREATE TABLE "Posts" (
-    "id" TEXT NOT NULL,
-    "title" TEXT NOT NULL,
-    "content" TEXT NOT NULL,
-    "createdOn" TIMESTAMP(3) NOT NULL DEFAULT CURRENT_TIMESTAMP,
-
-    CONSTRAINT "Posts_pkey" PRIMARY KEY ("id")
-);
