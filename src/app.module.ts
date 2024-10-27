4fd5d820a215d0c2e21d009b0b0ee5e82a124798
bijay-05 <hawkeyesniper69@gmail.com>
2024-10-27 22:45:03 +0545

    feat: add new controllers in application

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,9 +1,10 @@
 import { Module } from '@nestjs/common';
 import { PrismaModule } from './prisma/prisma.module';
 import { PostsModule } from './posts/posts.module';
+import { AuthModule } from './auth/auth.module';
 
 @Module({
-  imports: [PrismaModule, PostsModule],
+  imports: [PrismaModule, PostsModule, AuthModule],
   controllers: [],
   providers: [],
 })
